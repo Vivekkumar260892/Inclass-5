4e15a6023a0c5f309db9d1532c4f3677f8b8dfc3
Vivekkumar260892 <46549362+Vivekkumar260892@users.noreply.github.com>
2019-02-26 21:28:58 -0500

    Add files via upload

---
+++ Create dbo.Product_Sample.sql
@@ -0,0 +1,18 @@
+﻿USE [Inclass]
+GO
+
+/****** Object: Table [dbo].[Product_Sample] Script Date: 2/26/2019 9:08:26 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Product_Sample]
+(
+	[Product_Id] INT NOT NULL PRIMARY KEY, 
+    [Product_Name] VARCHAR(50) NULL
+);
+
+
+
---
+++ Create dbo.Sales.sql
@@ -0,0 +1,19 @@
+﻿USE [Inclass]
+GO
+
+/****** Object: Table [dbo].[Sales] Script Date: 2/26/2019 9:16:42 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Sales]
+(
+	[Bill Number] INT NOT NULL PRIMARY KEY, 
+    [Product Name] VARCHAR(50) NULL, 
+    [Product Id] int NOT NULL FOREIGN KEY REFERENCES Product_Sample(Product_Id)
+);
+
+
+
